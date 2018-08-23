9a3bcda313f1fdca1ef00f71c64c2cdb2f780548
Humna <humna.awan@rutgers.edu>
2018-08-22 17:12:47 -0700

    Added user-input. Now saving a readme too. Fixed filename for DD.

--- DC2visitGen/code/count_chips.py
+++ DC2visitGen/code/count_chips.py
@@ -7,7 +7,9 @@ import healpy as hp
 import pandas as pd
 import time
 import pickle
+import datetime
 
+import lsst.sims.maf
 import lsst.sims.maf.db as db
 import lsst.sims.maf.slicers as slicers
 from lsst.obs.lsstSim import LsstSimMapper
@@ -16,59 +18,98 @@ from lsst.sims.utils import _getRotSkyPos
 
 from lsst.sims.coordUtils import chipNameFromRaDec
 
+########################################################################
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option('-q', '--quiet',
+                  action= 'store_false', dest= 'verbose', default=True,
+                  help= 'Do not print stuff out')
+parser.add_option('--nside', dest='nside',
+                  help='HEALPix resolution parameter.',
+                  default=1024)
+parser.add_option('--surveyRegionTag', dest='surveyRegionTag',
+                  help='Either WFD or DD',
+                  default='WFD')
+parser.add_option('--dataFolder', dest='dataFolder',
+                  help='Folder name in DC2visitGen that contains the regionPixels and visitLists',
+                  default='DC2_Run2_Visits_nside1024')
+
+(options, args) = parser.parse_args()
+verbose = options.verbose
+nside = int(options.nside)
+surveyRegionTag = options.surveyRegionTag
+dataFolder = options.dataFolder
+
 ############################################################################################################
 dbpath = '/global/cscratch1/sd/awan/dbs_old_unzipped/minion_1016_desc_dithered_v3.db'
 pointingRACol = 'descDitheredRA'
 pointingDecCol = 'descDitheredDec'
 rotTelCol = 'descDitheredRotTelPos'
-surveyRegionTag = 'WFD'
 
-nside = 1024
-
-output_path = '/global/homes/a/awan/LSST/lsstRepos/DC2_visitList/DC2visitGen/DC2_Run2_Visits_nside1024/'
+if surveyRegionTag=='DD':
+    inRegionTag = 'uDD'
+else:
+    inRegionTag = surveyRegionTag
+output_path = '/global/homes/a/awan/LSST/lsstRepos/DC2_visitList/DC2visitGen/%s/'%dataFolder
 pixel_path = '%sregionPixels/DC2RegionPixels_minion1016_v3_%svisits_nside%s_%svisitsIn%s.csv'%(output_path, surveyRegionTag, nside,
-                                                                                               surveyRegionTag, surveyRegionTag)
+                                                                                               surveyRegionTag, inRegionTag)
 visits_path = '%svisitLists/DC2VisitList_minion1016_v3_%svisits_nside%s_%svisitsIn%s.csv'%(output_path, surveyRegionTag, nside,
-                                                                                           surveyRegionTag, surveyRegionTag)
-
+                                                                                           surveyRegionTag, inRegionTag)
+############################################################################################################
+def add_update(update, readme, verbose):
+    if verbose: print(update)
+    return '%s\n%s'%(readme,update)
 
 ############################################################################################################
-print('\nConnecting to the database ... ')
-print(dbpath)
+startTime_0 = time.time()
+if verbose:
+    print('Nside: %s\nsurveyRegionTag: %s\nData read in from %s'%(nside, surveyRegionTag, dataFolder))
+
+readme = '##############################\n%s'%(datetime.date.isoformat(datetime.date.today()))
+readme += '\nRunning with lsst.sims.maf.__version__: %s'%lsst.sims.maf.__version__
+readme += '\n\ncount_chips run:\ndataFolder= %s'%dataFolder
+readme += '\nnside: %s'%nside
+readme += '\nsurveyRegionTag: %s'%surveyRegionTag
+readme += '\ndbpath: %s'%dbpath
+readme += '\n%s, %s, %s'%(pointingRACol, pointingDecCol, rotTelCol)
+
+#----------------------------------------------------------------------------------------------
+readme = add_update(update='\nConnecting to the database ... \n%s'%dbpath, readme=readme, verbose=verbose)
+
 opsdb = db.OpsimDatabase(dbpath)
 propIds, propTags = opsdb.fetchPropInfo()
 sqlconstraint  = opsdb.createSQLWhere(surveyRegionTag, propTags)
-print('sqlconstraint: %s'%sqlconstraint)
+readme = add_update(update='sqlconstraint: %s'%sqlconstraint, readme=readme, verbose=verbose)
+
 colnames = ['fieldID', 'expMJD', 'obsHistID', pointingRACol, pointingDecCol, rotTelCol]
-print('Reading in: %s'%colnames)
+readme = add_update(update='Reading in: %s'%colnames, readme=readme, verbose=verbose)
 simdata = opsdb.fetchMetricData(colnames, sqlconstraint)
 
 #----------------------------------------------------------------------------------------------
-print('\nReading in regionPixels ... ')
+readme = add_update(update='\nReading in regionPixels ... ', readme=readme, verbose=verbose)
 regionPixels = pd.read_csv(pixel_path)
 regionPixels = np.array(regionPixels['regionPixels'])
 totPixels = len(regionPixels)
-print('%s pixels to consider.'%totPixels)
+readme = add_update(update='%s pixels to consider.'%totPixels, readme=readme, verbose=verbose)
 
 #----------------------------------------------------------------------------------------------
-print('\nSetting up the slicer ... ')
+readme = add_update(update='\nSetting up the slicer ... ', readme=readme, verbose=verbose)
 hpSlicer = slicers.HealpixSlicer(nside=nside, latLonDeg=opsdb.raDecInDeg, lonCol=pointingRACol, latCol=pointingDecCol)
 hpSlicer.setupSlicer(simdata)    # slice data: know which pixels are observed in which visit
 
 #----------------------------------------------------------------------------------------------
-print('\nSetting up the camera ... ')
+readme = add_update(update='\nSetting up the camera ... ', readme=readme, verbose=verbose)
 camera = LsstSimMapper().camera
 chipNames, obsIDs, fIDs, pixNums= [], [], [], []
 
 #----------------------------------------------------------------------------------------------
-print('\nStarting the chip count ... ')
+readme = add_update(update='\nStarting the chip count ... ', readme=readme, verbose=verbose)
 prevPercent = 0.
 startTime = time.time()
 for p, pixel in enumerate(regionPixels):  # run over all the pixels in the region
     pixRA, pixDec = hpSlicer._pix2radec(pixel)    # radians returned
     indObsInPixel = hpSlicer._sliceSimData(pixel)   # indices in simData for when an observation
                                                     # happened in this pixel
-
     for index in indObsInPixel['idxs']:
         # get data from simdata for identifying each visit
         obsID = simdata[index]['obsHistID']
@@ -98,19 +139,22 @@ for p, pixel in enumerate(regionPixels):  # run over all the pixels in the regio
 
     percentDone = 100.*(p+1)/totPixels
     delPercent = percentDone-prevPercent
-    if (delPercent>1):
-        print('%f%% pixels done\nTime passed (min): %f\n'%(percentDone, (time.time()-startTime)/60.))
+    if (delPercent>5):
+        readme = add_update(update='%f%% pixels done\nTime passed (min): %f\n'%(percentDone, (time.time()-startTime)/60.),
+                            readme=readme, verbose=verbose)
         prevPercent = percentDone
     #if (percentDone>1.):
     #    break
-print('\nAll done.\nTime passed (min): %f\n'%((time.time()-startTime)/60.))
+readme = add_update(update='\nAll done.\nTime passed (min): %f\n'%((time.time()-startTime)/60.),
+                    readme=readme, verbose=verbose)
 
 #----------------------------------------------------------------------------------------------
 obsIDs, fIDs, chipNames, pixNums = np.array(obsIDs), np.array(fIDs), np.array(chipNames), np.array(pixNums)
-print('Unique obsHistIDs: %d \n## Unique chipNames: %d \n'%(len(np.unique(obsIDs)), len(np.unique(chipNames))))
+readme = add_update(update='Unique obsHistIDs: %d \n## Unique chipNames: %d \n'%(len(np.unique(obsIDs)), len(np.unique(chipNames))),
+                    readme=readme, verbose=verbose)
 
 #  get rid of repeated entries; consolidate the data from unique observations.
-print('Consolidating the data ... ')
+readme = add_update(update='Consolidating the data ... ', readme=readme, verbose=verbose)
 obsIDsList, fIDsList, chipNamesList, pixNumsList = [], [], [], []
 for obs in np.unique(obsIDs):
     obsIDsList.append(obs)
@@ -124,27 +168,36 @@ numChips= []
 for i in range(len(obsIDsList)):
     numChips.append(len(chipNamesList[i]))
     
-print('Min, Max number of chips added by any given visit: %d, %s'%( min(numChips), max(numChips)))
-print('Total number of chips (across all visits to be simulated): %d'%sum(numChips))
+readme = add_update(update='Min, Max number of chips added by any given visit: %d, %s'%( min(numChips), max(numChips)),
+          readme=readme, verbose=verbose)
+readme = add_update(update='Total number of chips (across all visits to be simulated): %d'%sum(numChips),
+          readme=readme, verbose=verbose)
 
 #----------------------------------------------------------------------------------------------
 out = pd.read_csv(visits_path)
 if np.array(out['obsHistID'])!=obsIDsList:
-    print('\n##ObsIDs do not match.')
-    plt.clf()
-    plt.hist(out['obsHistID'], label='saved before', histtype='step')
-    plt.hist(obsIDsList, label='calculated here', histtype='step')
-    plt.xlabel('Bins of obsIDs')
-    plt.ylabel('Counts')
-    plt.legend()
-    plt.show()
+    readme = add_update(update='\n##ObsIDs do not match.', readme=readme, verbose=verbose)
+    if verbose:
+        plt.clf()
+        plt.hist(out['obsHistID'], label='saved before', histtype='step')
+        plt.hist(obsIDsList, label='calculated here', histtype='step')
+        plt.xlabel('Bins of obsIDs')
+        plt.ylabel('Counts')
+        plt.legend()
+        plt.show()
 else:
-    print('\nObsIDs match.')
+    readme = add_update(update='\n##ObsIDs do match.', readme=readme, verbose=verbose)
 #----------------------------------------------------------------------------------------------
-# save the data
+# save the data + readme
 dataToSave = {'obsHistID': obsIDsList, 'fIDs': fIDsList, 'chipNames': chipNamesList}
-filename = 'chipPerVisitData_%s_nside%s_%dNonWFChipsToSimulate.pickle'%(surveyRegionTag, nside, sum(numChips))
+filename = 'chipPerVisitData_%s_nside%s_%dNonWaveFrontChipsToSimulate.pickle'%(surveyRegionTag, nside, sum(numChips))
 with open('%s/%s'%(output_path, filename), 'wb') as handle:
     pickle.dump(dataToSave, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-print('\nSaved %s in %s\n'%(filename, output_path))
+readme = add_update(update='\nSaved %s in %s\n'%(filename, output_path), readme=readme, verbose=verbose)
+
+readme += '\nTotal time taken: %.2f (min)\n\n'%((time.time()-startTime_0)/60.)
+
+readme_file= open('%s/chipcount_readme_%s_nside%s_%schips.txt'%(output_path, surveyRegionTag, nside, sum(numChips)), 'a')
+readme_file.write(readme)
+readme_file.close()
