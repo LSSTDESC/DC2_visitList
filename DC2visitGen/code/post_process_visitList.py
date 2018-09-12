89d264378040cd610ee7deec6df5641a77f1d910
Humna <humna.awan@rutgers.edu>
2018-09-11 17:24:43 -0700

    Added the functionality to group by year. #8

--- DC2visitGen/code/post_process_visitList.py
+++ DC2visitGen/code/post_process_visitList.py
@@ -8,7 +8,7 @@ import lsst.sims.maf
 __all__ = ['post_process_visit_lists']
 
 def post_process_visit_lists(dir_to_readfrom, wfd_filename, uddf_filename, base_filetag, outDir,
-                             saveData=True, nVisits=None,
+                             saveData=True, nVisits=None, group_by_year=False,
                              dbpath='/global/cscratch1/sd/awan/dbs_old_unzipped/minion_1016_desc_dithered_v3.db'):
     """
 
@@ -36,7 +36,7 @@ def post_process_visit_lists(dir_to_readfrom, wfd_filename, uddf_filename, base_
 
     # read in the columns from the full database
     opsdb = db.OpsimDatabase(dbpath)
-    colnames=['obsHistID', 'expMJD']
+    colnames = ['obsHistID', 'expMJD', 'night']
     simdata = opsdb.fetchMetricData(colnames=colnames, sqlconstraint=None, groupBy='expMJD')
 
     # need obsHistID as an index (easier to match stuff) and as a column itself (easier to save things)
@@ -63,22 +63,47 @@ def post_process_visit_lists(dir_to_readfrom, wfd_filename, uddf_filename, base_
         else:
             df = visit_files[surveytag].join(simdata_pd).sort_values(by='expMJD')
 
+        if saveData: print('Outdir: %s\n'%outDir)
         # group by band and save obsHistID and expMJD
         groups = df.groupby('band')
         for band in groups.groups.keys():
+            print('------------------------------------------------------------------------')
             print('Working with %s-band'%band)
-            subset = groups.get_group(band)[['obsHistID', 'expMJD']]
-            print('%s visits for %s-band.'%(len(subset), band))
-            tot += len(subset)
-            if saveData:
-                # save the file
-                filename = '%s_%s-band_%svisits.txt'%(base_filetag, band, surveytag)
-                subset.to_csv('%s/%s'%(outDir, filename), index=False, sep=' ', header=['obsHistID', 'expMJD'])
-                print('\nSaved %s\nin %s.\n'%(filename, outDir))
 
-            to_return[surveytag][band] = subset
+            if group_by_year:
+                to_return[surveytag][band] = {}
+                subset = groups.get_group(band)
+                print('%s visits for %s-band.'%(len(subset), band))
+                subset['year'] = [int(np.floor(f/365.25))+1 for f in subset['night']]
 
-        print('Total %s visits saved.\n'%tot)
+                groups_yr = subset.groupby('year')
+
+                for yr in groups_yr.groups.keys():
+                    subset_yr = groups_yr.get_group(yr)[['obsHistID', 'expMJD']]
+                    print('%s visits for year %s.'%(len(subset_yr), yr))
+                    tot += len(subset_yr)
+
+                    if saveData:
+                        # save the file
+                        filename = '%s_%s-band_Y%s_%svisits.txt'%(base_filetag, band, yr, surveytag)
+                        subset_yr.to_csv('%s/%s'%(outDir, filename), index=False, sep=' ', header=['obsHistID', 'expMJD'])
+                        print('\nSaved %s.\n'%(filename))
+
+                    to_return[surveytag][band][yr] = subset_yr
+
+            else:
+                subset = groups.get_group(band)[['obsHistID', 'expMJD']]
+                print('%s visits for %s-band.'%(len(subset), band))
+                tot += len(subset)
+                if saveData:
+                    # save the file
+                    filename = '%s_%s-band_%svisits.txt'%(base_filetag, band, surveytag)
+                    subset.to_csv('%s/%s'%(outDir, filename), index=False, sep=' ', header=['obsHistID', 'expMJD'])
+                    print('\nSaved %s\n'%(filename))
+
+                to_return[surveytag][band] = subset
+
+        print('Total %s %s visits saved.\n'%(tot, surveytag))
 
     print('All done. Time taken: %.2f s'%(time.time()-startTime))
 
